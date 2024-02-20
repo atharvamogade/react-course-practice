fa613a663488c9149d05949c7c80a671dcce7218
atharvamogade <atharvamogade12@gmail.com>
2024-02-20 16:13:36 +0530

    Creat reusable component for step message using child props

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -45,9 +45,10 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
+          {/* <p className="message">
             Step {step}: {messages[step - 1]}
-          </p>
+          </p> */}
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button
               textColor={"#fff"}
@@ -62,6 +63,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
