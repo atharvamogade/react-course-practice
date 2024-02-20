f0cf910c313d976a4c26a1685bd8b6ba559b42be
atharvamogade <atharvamogade12@gmail.com>
2024-02-20 12:37:22 +0530

    Add sorting options for the list

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -90,10 +90,24 @@ function Form({ onAddItems }) {
 }
 
 function PackingList({ items, onDeleteItems, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => {
+        {sortedItems.map((item) => {
           // return <p>{item.description}</p>;
           return (
             <Item
@@ -105,6 +119,18 @@ function PackingList({ items, onDeleteItems, onToggleItem }) {
           );
         })}
       </ul>
+
+      <div
+        className="actions"
+        value={sortBy}
+        onChange={(e) => setSortBy(e.target.value)}
+      >
+        <select>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
