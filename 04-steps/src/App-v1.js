9210b6a94dad79a7c0d1485ad4978977a94387f1
atharvamogade <atharvamogade12@gmail.com>
2024-02-20 16:04:36 +0530

    04: Create reusable button component and place content in component instances using children prop which is a predefined keyword in react

--- 04-steps/src/App-v1.js
+++ 04-steps/src/App-v1.js
@@ -10,6 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
+      <Steps />
     </div>
   );
 }
@@ -49,30 +50,21 @@ function Steps() {
             Step {step}: {messages[step - 1]}
           </p>
           <div className="buttons">
-            <Button
-              textColor={"#fff"}
-              bgColor={"#7950f2"}
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
             >
-              <span>👈</span> Previous
-            </Button>
-            <Button textColor={"#fff"} bgColor={"#7950f2"} onClick={handleNext}>
-              Next <span>👉</span>
-            </Button>
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
